bc910452c1a98333a575326364926fb843d444d5
raymond zhang <raymond.zhang@bellmedia.ca>
2020-02-11 16:39:18 -0500

    RabbitMQ publisher

--- db/rabbitmq.go
+++ db/rabbitmq.go
@@ -13,6 +13,19 @@ type RabbitMQ struct {
 	config *config.Config
 }
 
+func (rabbitMQ *RabbitMQ) declareQueue(channel *amqp.Channel) (amqp.Queue, error) {
+	q, err := channel.QueueDeclare(
+		"superdispatcher_dev",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	rabbitMQ.logError(err, "Failed to declare a queue")
+	return q, err
+}
+
 // NewRabbitMQ creates RabbitMQ client instance
 func NewRabbitMQ(cfg *config.Config) (*RabbitMQ, error) {
 	var rabbit = &RabbitMQ{}
@@ -20,15 +33,48 @@ func NewRabbitMQ(cfg *config.Config) (*RabbitMQ, error) {
 	return rabbit, nil
 }
 
-func (rabbitMQ *RabbitMQ) Dial() (*amqp.Connection, error) {
+func (rabbitMQ *RabbitMQ) logError(err error, message string) {
+	rabbitMQ.config.Logger.Error(err, message)
+}
+
+func (rabbitMQ *RabbitMQ) Connect() (*amqp.Connection, error) {
 	cfg := rabbitMQ.config.Constants.RabbitMQ
 	connString := fmt.Sprintf("amqp://%s:%s@%s:%d/", cfg.User, url.PathEscape(cfg.Password), cfg.Host, cfg.Port)
 	conn, err := amqp.Dial(connString)
-	if err != nil {
-		rabbitMQ.config.Logger.Error(err, fmt.Sprintf("failed to establish RabbitMQ connection: %+v", connString))
-		return nil, err
-	}
-	defer conn.Close()
-
+	rabbitMQ.logError(err, fmt.Sprintf("failed to establish RabbitMQ connection: %+v", connString))
 	return conn, err
+}
+
+func (rabbitMQ *RabbitMQ) Publish(conn *amqp.Connection, name string, message string) error {
+	ch, err := conn.Channel()
+	rabbitMQ.logError(err, "Failed to open a channel")
+	defer ch.Close()
+	q, err := rabbitMQ.declareQueue(ch)
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	rabbitMQ.logError(err, fmt.Sprintf("Failed publish: %+v", message))
+	return err
+}
+
+func (rabbitMQ *RabbitMQ) Consume(conn *amqp.Connection, name string, message string) error {
+	ch, err := conn.Channel()
+	rabbitMQ.logError(err, "Failed to open a channel")
+	defer ch.Close()
+	q, err := rabbitMQ.declareQueue(ch)
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
 }
--- db/rabbitmq_test.go
+++ db/rabbitmq_test.go
@@ -8,12 +8,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListenDial(t *testing.T) {
+func TestPublish(t *testing.T) {
 	config, err := config.New("config", "../resources")
 	assert.Nil(t, err)
 	rabbitMQ, err := NewRabbitMQ(config)
 	assert.Nil(t, err)
-	_, err = rabbitMQ.Dial()
-
+	conn, err := rabbitMQ.Connect()
+	defer conn.Close()
+	rabbitMQ.Publish(conn, "Go Publish", "Raymond Test")
 	assert.Nil(t, err)
 }
